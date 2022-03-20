950f2d5c296c5d1c0dfdcbe03737dd6127c06600
Kimathi Kaai <kiprotich.kaai@gmail.com>
2022-03-20 17:02:52 -0400

    WIP: Refactored model, training and data loading

--- src/model.py
+++ src/model.py
@@ -1,4 +1,3 @@
-import pdb
 import typing
 
 import pytorch_lightning as pl
@@ -8,67 +7,86 @@ from torchmetrics.functional import accuracy
 
 
 class CRNNModel(pl.LightningModule):
-    """
-    Model class for CRNN
-    """
-
     def __init__(self, **kwargs):
-        """
-        Initializes the network
-        """
-
         super().__init__()
+
+        # parameters
+        self.batch_size = kwargs.get("batch_size")
+        self.lr = kwargs.get("learning_rate")
+        self.lstm_dropout = kwargs.get("lstm_dropout")
+        self.num_classes = kwargs.get("num_classes")
+        self.lstm_hidden_size = kwargs["hidden_size"]
+        self.lstm_layers = kwargs["lstm_layers"]
+        self.input_shape = kwargs["input_shape"]
+        self.device = kwargs["device"]
+        self.save_hyperparameters()
+
+        # must be defined for logging computational graph
+        self.example_input_array = torch.rand((1, *self.input_shape))
 
         # Define loss function
         self.loss = nn.CrossEntropyLoss()
 
-        # Define other model parameters
-        self.lr = kwargs["learning_rate"]
-        self.lstm_dropout = kwargs['lstm_dropout']
-        self.lstm_hidden_size = 100
-        self.num_classes = 7
+        self.conv1 = nn.Conv1d(
+            in_channels=self.input_shape[0], out_channels=128, kernel_size=7
+        )
+        self.relu1 = nn.ReLU(True)
+        self.pool1 = nn.MaxPool1d(2)
 
-        # TODO: Initialize model architecture
-        self.cnn = nn.Sequential()
-        # conv1
-        self.cnn.add_module(
-            "conv1", nn.Conv1d(in_channels=6, out_channels=128, kernel_size=7)
-        )
-        self.cnn.add_module("relu1", nn.ReLU(True))
-        self.cnn.add_module("pooling1", nn.MaxPool1d(2))
-        # conv2
-        self.cnn.add_module(
-            "conv2", nn.Conv1d(in_channels=128, out_channels=256, kernel_size=7)
-        )
-        self.cnn.add_module("relu2", nn.ReLU(True))
-        self.cnn.add_module("pooling2", nn.MaxPool1d(2))
-        # rnn
-        self.rnn = nn.LSTM(
+        self.conv2 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=7)
+        self.relu2 = nn.ReLU(True)
+        self.pool2 = nn.MaxPool1d(2)
+
+        self.lstm = nn.LSTM(
             input_size=256,
             hidden_size=self.lstm_hidden_size,
-            num_layers=2,
+            num_layers=self.lstm_layers,
             batch_first=True,
             bidirectional=True,
-            dropout = self.lstm_dropout
+            dropout=self.lstm_dropout,
         )
-        # out
-        self.fc = nn.Linear(
-            in_features=self.lstm_hidden_size * 2, out_features=self.num_classes
+
+        self.lin1 = nn.Linear(
+            in_features=self.lstm_hidden_size * self.lstm_layers,
+            out_features=self.num_classes,
         )
+
+        self.sm = nn.Softmax(dim=1)
 
     def forward(self, x: typing.Any) -> typing.Any:
-        """
-        Forward pass through the CRNN model.
+        if x.shape[1] != self.input_shape[0]:
+            x = x.permute(0, 2, 1)
 
-        :param x: Input to CRNN model
+        assert x.shape[1] == self.input_shape[0]
 
-        :return: Result of CRNN model inference on input
-        """
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
 
-        x_cnn = self.cnn(x.permute(0, 2, 1))
-        x_permute = x_cnn.permute(0, 2, 1)
-        x_rnn, _ = self.rnn(x_permute)
-        return self.fc(x_rnn[:, -1, :])  # grab the last sequence
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+
+        x = x.permute(0, 2, 1)
+        x, _ = self.lstm(x)
+
+        x = x[:, -1, :]  # grab the last sequence
+        x = self.lin1(x)
+
+        return x
+
+    def get_class(self, x):
+        softmax = self.sm(x)
+        classification = torch.argmax(softmax, dim=1)
+        return classification
+
+    def spark_predict(self, x):
+        # create batch dimension
+        x = torch.unsqueeze(x, dim=0)
+
+        x = self.forward(x)
+
+        return self.get_class(x)
 
     def configure_optimizers(self):
         """
@@ -79,9 +97,7 @@ class CRNNModel(pl.LightningModule):
 
         return optimizer
 
-    def training_step(
-        self, batch: typing.Any, batch_idx: int
-    ) -> typing.Union[torch.Tensor, typing.Dict[str, typing.Any]]:
+    def training_step(self, batch: typing.Any, batch_idx: int):
         """
         Compute and return the training loss.
 
@@ -90,28 +106,24 @@ class CRNNModel(pl.LightningModule):
         :return: Loss tensor or a dictionary
         """
 
-        x = batch.get("timeseries").cuda()
-        y = batch.get("label").cuda()
+        x = batch.get("timeseries").to(self.device)
+        y_gt = batch.get("label").to(self.device)
+
+        assert batch.shape[1:] == self.input_shape
 
         # forward
-        x_hat = self.cnn(x.permute(0, 2, 1))
-        x_hat = x_hat.permute(0, 2, 1)
-        x_hat, _ = self.rnn(x_hat)
-        predictions = self.fc(x_hat[:, -1, :])
+        y_pred = self.forward(x)
+        y_pred_softmax = self.get_class(y_pred)
 
-        loss = self.loss(predictions, y.float())
-        _, y_hat = torch.max(predictions, dim=1)
-        _, y = torch.max(y, dim=1)
-        acc = accuracy(y_hat, y)
+        loss = self.loss(y_pred, y_gt.float())
+        acc = accuracy(y_pred_softmax, y_gt)
 
         self.log("train_loss", loss)
         self.log("train_acc", acc)
 
-        return {"loss": loss}
+        return loss
 
-    def validation_step(
-        self, batch: typing.Any, batch_idx: int
-    ) -> typing.Union[torch.Tensor, typing.Dict[str, typing.Any], None]:
+    def validation_step(self, batch: typing.Any, batch_idx: int):
         """
         Compute and return the validation loss and accuracy.
 
@@ -119,50 +131,19 @@ class CRNNModel(pl.LightningModule):
 
         :return: Loss tensor, a dictionary, or None
         """
-        x = batch.get("timeseries")
-        y = batch.get("label")
+        x = batch.get("timeseries").to(self.device)
+        y_gt = batch.get("label").to(self.device)
+
+        assert batch.shape[1:] == self.input_shape
 
         # forward
-        x_hat = self.cnn(x.permute(0, 2, 1))
-        x_hat = x_hat.permute(0, 2, 1)
-        x_hat, _ = self.rnn(x_hat)
-        predictions = self.fc(x_hat[:, -1, :])
+        y_pred = self.forward(x)
+        y_pred_softmax = self.get_class(y_pred)
 
-        loss = self.loss(predictions, y.float())
-        _, y_hat = torch.max(predictions, dim=1)
-        _, y = torch.max(y, dim=1)
-        acc = accuracy(y_hat, y)
+        loss = self.loss(y_pred, y_gt.float())
+        acc = accuracy(y_pred_softmax, y_gt)
 
-        self.log("val_acc", acc, prog_bar=True)
         self.log("val_loss", loss)
+        self.log("val_acc", acc)
 
-        return {"val_loss": loss, "val_acc": acc}
-
-    def test_step(
-        self, batch: typing.Any
-    ) -> typing.Union[torch.Tensor, typing.Dict[str, typing.Any], None]:
-        """
-        Compute and return the test accuracy.
-
-        :param batch: The output of the test Dataloader
-
-        :return: Loss tensor, a dictionary, or None
-        """
-
-        x = batch.get("timeseries")
-        y = batch.get("label")
-
-        # forward
-        x_hat = self.cnn(x)
-        x_hat = x_hat.permute(0, 2, 1)
-        x_hat, _ = self.rnn(x_hat)
-        predictions = self.fc(x_hat[:, -1, :])
-
-        loss = self.loss(predictions, y)
-        _, y_hat = torch.max(predictions, dim=1)
-        _, y = torch.max(y, dim=1)
-        acc = accuracy(y_hat, y)
-
-        self.log("test_acc", acc, on_epoch=True)
-
-        return {"test_acc": acc}
+        return loss
--- src/train.py
+++ src/train.py
@@ -1,64 +1,103 @@
-from argparse import ArgumentParser
+import datetime
 import os
+from argparse import ArgumentParser
 
-from data_module import ShoulderExerciseDataModule
 import mlflow
-from model import CRNNModel
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.profiler import SimpleProfiler
 
-# deterministic
-pl.seed_everything(42)
+from data_module import ShoulderExerciseDataModule
+from model import CRNNModel
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="PyTorch Category Classifier")
 
     # General model paramters
-    parser.add_argument("--learning-rate", type=float, help="Learning rate for model optimization")
-    parser.add_argument("--lstm-dropout", type=float, help="Dropout for for lstm layers")
-    parser.add_argument("--num-workers", type=int, help="Number of data loader workers")
+    parser.add_argument("--learning_rate", type=float)
+    parser.add_argument("--lstm_dropout", type=float)
+    parser.add_argument("--lstm_layers", type=int, default=2)
+    parser.add_argument("--lstm_hidden_size", type=float, default=100)
+    parser.add_argument("--num_workers", type=int)
+    parser.add_argument("--num_classes", type=int, default=7)
 
     # Dataset paramters
-    parser.add_argument("--window-size", type=int, help="Number of timesteps per window")
-    parser.add_argument("--window-stride", type=int, help="Difference between consequtive windows")
-    parser.add_argument("--data-dir", type=str, help="Location to store dataset samples")
-    parser.add_argument("--batch-size", type=int, help="Training batch size")
+    parser.add_argument("--input_shape", type=tuple, default=(6, 100))
+    parser.add_argument("--window_size", type=int)
+    parser.add_argument("--window_stride", type=int)
+    parser.add_argument("--dataloader_source", type=str, default="./tmp/spardata")
+    parser.add_argument("--dataloader_temp", type=str, default="./datasets/spar_csv")
+    parser.add_argument("--batch_size", type=int)
 
     # Early stopping parameters
-    parser.add_argument("--es-monitor", type=str, help="Early stopping monitor parameter")
-    parser.add_argument("--es-mode", type=str, help="Early stopping mode parameter")
-    parser.add_argument("--es-verbose", type=bool, help="Early stopping verbose parameter")
-    parser.add_argument("--es-patience", type=int, help="Early stopping patience parameter")
+    parser.add_argument("--es_monitor", type=str, default="val_loss")
+    parser.add_argument("--es_mode", type=str, default="min")
+    parser.add_argument("--es_patience", type=int)
+
+    # Other parameters
+    parser.add_argument("--device", default="cuda", choices=["cuda", "cpu"])
+    parser.add_argument("--seed", default=42, type=int)
+    parser.add_argument("--logdir", default="./", type=str)
 
     parser = pl.Trainer.add_argparse_args(parent_parser=parser)
 
     args = parser.parse_args()
     dict_args = vars(args)
 
-    # Autolog parameters, metrics and artifacts to MLflow
-    mlflow.pytorch.autolog()
+    # Choose train and validation splits
+    dict_args["load_csv_file_patterns"] = {
+        "train": [
+            "**/spar_csv/S[1-9]_*.csv",
+            "**/spar_csv/S1[0-6]_*.csv",
+        ],
+        "validation": [
+            "**/spar_csv/S1[7-9]_*.csv",
+            "**/spar_csv/S20_*.csv",
+        ],
+    }
+
+    pl.seed_everything(dict_args["seed"])
 
     # Initialize CRNN model to train
-    model = CRNNModel(**dict_args).to(device='cuda')
+    model = CRNNModel(**dict_args).to(device=dict_args["device"])
 
     # Initialize data module
     data_module = ShoulderExerciseDataModule(**dict_args)
 
-    early_stopping = EarlyStopping(
+    # Callback: early stopping parameters
+    early_stopping_callback = EarlyStopping(
         monitor=dict_args["es_monitor"],
         mode=dict_args["es_mode"],
-        verbose=dict_args["es_verbose"],
+        verbose=True,
         patience=dict_args["es_patience"],
     )
 
+    # Callback: model checkpoint strategy
     checkpoint_callback = ModelCheckpoint(
-        dirpath=os.getcwd(), save_top_k=1, verbose=True, monitor="val_loss", mode="min"
+        save_top_k=1,
+        verbose=True,
+        monitor=dict_args["es_monitor"],
+        mode=dict_args["es_mode"],
     )
 
+    # Trainer: initialize training behaviour
+    profiler = SimpleProfiler()
+    now = datetime.datetime.now().strftime("%m%d%H%M")
+    logger = TensorBoardLogger(
+        save_dir=dict_args["logdir"],
+        version=now,
+        name="lightning_logs",
+        log_graph=True,
+    )
     trainer = pl.Trainer.from_argparse_args(
-        args, callbacks=[early_stopping, checkpoint_callback], gpus=1, deterministic=True
+        callbacks=[early_stopping_callback, checkpoint_callback],
+        gpus=1 if dict_args["device"] == "cuda" else 0,
+        deterministic=True,
+        profiler=profiler,
+        logger=logger,
     )
 
+    # Trainer: train model
     trainer.fit(model, data_module)
-    # trainer.test(model, datamodule=data_module)
