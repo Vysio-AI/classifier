772f7a3f3cd1854256efe6deb29d508b5dd39732
Kimathi Kaai <kiprotich.kaai@gmail.com>
2022-03-23 20:02:25 -0400

    squash! Moving to 6 class and saving onnx model

---
+++ src/onnx_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import onnx
+import onnxruntime
+
+file_path = (
+    "./save_logs/03_Wed_19_49_15/model_03_Wed_19_49_15.onnx"
+)
+ort_session = onnxruntime.InferenceSession(file_path)
+input_name = ort_session.get_inputs()[0].name
+ort_inputs = {input_name: np.random.randn(1, 6, 100).astype(np.float32)}
+ort_outs = ort_session.run(None, ort_inputs)[0]
+print(ort_outs) # will need to use argmax to get class
+print(type(ort_outs)) # numpy array
+print(ort_outs.shape) # (1, num_class)
